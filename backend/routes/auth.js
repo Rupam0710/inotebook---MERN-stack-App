bedaf998fb1d1f73b613b7eb044f4ff103b05b8d
Rupam <rupampal48@gmail.com>
2023-06-10 23:19:35 +0530

    49 commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body,validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'RupamisfuckingA8some';
 
 
 //Create a user using : POST "/api/auth/createuser". No login required
@@ -22,13 +26,26 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({errors: "Sorry a user with this email already exists" });
     }
+    
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password,salt);
+
     //create a new user
     user = await User.create({
         name : req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
     })
-    res.json(user);
+    
+    const data = {
+        user :{
+            id:user.ud,
+        }
+    }
+    const auth_token = jwt.sign(data,JWT_SECRET);
+    // console.log(auth_token);
+    res.json({auth_token});
+
     } catch(error){
         console.error(error.message);
         res.status(500).send("Some error occured");
