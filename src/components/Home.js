3fca70285725eb8dc3f4542e35032596e0088034
Rupam <rupampal48@gmail.com>
2023-08-11 22:21:57 +0530

    61st commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
-import React ,{useContext} from "react";
-import noteContext from "../context/notes/noteContext";
+
+import Notes from "./Notes";
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const {notes,setNotes} = context;
+  
   return (
     <div>
       <div className="container my-3">
@@ -42,13 +41,9 @@ const Home = () => {
         </button>
       </form>
       </div>
-
-      <div className="container my-3">
-      <h2>Your notes</h2>
-      {notes.map((note)=>{
-        return note.title;
-      })}
-      </div>
+      
+      <Notes/>
+      
     </div>
   );
 };
